8fab2c3b8ea80988b00d62677937e17de08d0c65
Redeemer Ntumy <redeemer@Redeemers-MacBook-Air.local>
2022-05-24 01:42:38 +0000

    Seperating classes

--- Home.py
+++ Home.py
@@ -1,20 +1,19 @@
 from PyQt6.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QVBoxLayout,QMainWindow
 from PyQt6.QtGui import QIcon,QFont,QColor
+from PyQt6.QtCore import Qt
 import sys
-
-
-class ChooseWindow(QWidget,QColor):  
-    def __init__(self):
-        super().__init__()                                     
-        self.setWindowTitle("New Screen")
+import ServerorClient
 
 class Window(QWidget,QColor):
     def __init__(self):
         super().__init__()
+        self.show()
         self.main_window()
+
     
     def main_window(self):
         self.label_start = QLabel("Establish an MPI cluster within a LAN with ease", self)
+        self.label_start.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.label_start.setFont(QFont("Arial",20))
         self.vbox=QVBoxLayout()
         self.setWindowTitle("PMPI Server")
@@ -24,14 +23,10 @@ class Window(QWidget,QColor):
         self.vbox.addWidget(self.label_start)
         self.vbox.addWidget(self.btn_start)
         self.setLayout(self.vbox)
-        self.btn_start.clicked.connect(self.window2)
-        self.show()
+        self.chooseWindow=ServerorClient.ChooseWindow()
+        self.btn_start.clicked.connect(self.chooseWindow.serverclient)
+    
 
-    def window2(self):  
-        self.w=ChooseWindow()
-        self.setGeometry(450,200,600,500)                                          
-        self.w.show()
-        self.hide()
     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ ServerorClient.py
@@ -0,0 +1,38 @@
+from ctypes import alignment
+from PyQt6.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QVBoxLayout,QMainWindow
+from PyQt6.QtGui import QColor,QFont
+from PyQt6.QtCore import Qt
+import sys
+import Home
+
+
+class ChooseWindow(QWidget,QColor):  
+    def __init__(self):
+        super().__init__()                                     
+        self.setWindowTitle("Client or Server")
+        self.question = QLabel("Would you like this PC to be the Server or the Client?", self)
+        self.question.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.question.setFont(QFont("Arial",20))
+        self.setGeometry(450,200,600,500)
+        self.vbox=QVBoxLayout()
+        self.server=QPushButton("Server",self)
+        self.client=QPushButton("Client",self)
+        self.server.setStyleSheet('background-color: green')
+        self.client.setStyleSheet('background-color: blue')
+        self.vbox.addWidget(self.question)
+        self.vbox.addWidget(self.server)
+        self.vbox.addWidget(self.client)
+        self.setLayout(self.vbox)
+        
+
+    def serverclient(self):  
+        self.serclient=ChooseWindow()                                         
+        self.serclient.show()
+        
+        
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Home.Window()
+    sys.exit(app.exec())
