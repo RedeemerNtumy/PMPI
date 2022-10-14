0b1635daa2018c57b271c49285cbf5708b121178
Redeemer Ntumy <55901355+RedeemerNtumy@users.noreply.github.com>
2022-10-14 02:01:39 +0000

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-# #!/bin/bash
-# read -p 'Server username: ' uservar
-# read -sp 'Password: ' pwvar
-# for server in `cat list_of_servers`; do
-#     sshpass -p "${pwvar}" ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no "${uservar}@${server}"
-# done
